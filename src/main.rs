73d72bf4720cd75de791753aabf94f950cf10491
DrJones117 <github.finished102@passinbox.com>
2025-08-02 11:10:27 -0500

    line attempt 3

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,10 @@ fn main() {
         tga::ImageType::UncompressedTrueColor,
         tga::ColorType::RGB,
     );
-    draw_line(13, 20, 80, 40, &mut image, &white);
-    draw_line(20, 13, 40, 80, &mut image, &color);
-    draw_line(80, 40, 13, 20, &mut image, &red);
+
+    draw_line(&mut 13, &mut 20, &mut 80, &mut 40, &mut image, &white);
+    draw_line(&mut 20, &mut 13, &mut 40, &mut 80, &mut image, &red);
+    draw_line(&mut 80, &mut 40, &mut 13, &mut 20, &mut image, &color);
 
     match image.write_to_file("test.tga") {
         Ok(()) => (),
@@ -24,11 +25,33 @@ fn main() {
 }
 
 // First iteration of drawing the line
-fn draw_line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut tga::Image, color: &tga::Color) {
-    for x in x0..x1 {
-        let t = (x - x0) as f32 / (x1 - x0) as f32;
-        let y = (y0 as f32 * (1.0 - t)) + (y1 as f32 * t);
-
-        image.set(x as u16, y as u16, color);
+fn draw_line(
+    x0: &mut i32,
+    y0: &mut i32,
+    x1: &mut i32,
+    y1: &mut i32,
+    image: &mut tga::Image,
+    color: &tga::Color,
+) {
+    let mut steep = false;
+    if (*x1 - *x0).abs() < (*y1 - *y0).abs() {
+        //if our line is steep, ie dx < dy, we can transpose the image
+        std::mem::swap(x0, y0);
+        std::mem::swap(x1, y1);
+        steep = true;
+    }
+    if *x0 > *x1 {
+        std::mem::swap(x0, x1);
+        std::mem::swap(y0, y1);
+    }
+    for x in *x0..*x1 {
+        let t = (x - *x0) as f32 / (*x1 - *x0) as f32;
+        let y = (*y0 as f32 * (1.0 - t)) + (*y1 as f32 * t);
+        if steep {
+            // detranspose the image
+            image.set(y as u16, x as u16, color);
+        } else {
+            image.set(x as u16, y as u16, color);
+        }
     }
 }
