46b415f844e1bd97350e895cb020df362ebd46ec
Jonah Morgan <github.finished102@passinbox.com>
2025-08-20 22:45:08 -0500

    added test files

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -5,4 +5,6 @@ pub mod triangle_raster;
 pub use algorithms::Algorithms;
 pub use line_raster::bresenhams_line_alg;
 pub use line_raster::line_alg_with_floats;
+
 pub use triangle_raster::rasterize_triangle;
+pub use triangle_raster::rasterize_triangle_scanline;
